9499974a55433c5d2e022d14fb4cc39a1b21decc
Rohan <rohanasif1990@gmail.com>
2022-10-16 02:25:49 +0500

    corrected whitespace

--- js/app.js
+++ js/app.js
@@ -22,6 +22,7 @@
  * Define Global Variables
  * 
 */
+
 const headings = document.querySelectorAll("h2");
 const ul = document.querySelector("ul");
 const sections = document.querySelectorAll("section");
@@ -60,6 +61,7 @@ for (let i = 0; i < headings.length; i++) {
 const anchors = document.querySelectorAll("a");
 
 // Add class 'active' to section when near top of viewport
+
 for (let section of sections) {
   if (isNearTop(section) && !(isActive(section))) {
     section.classList.add("your-active-class");
